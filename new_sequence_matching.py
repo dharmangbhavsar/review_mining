6a1983b17513ff40c942fdacf22cd1ea855b5c12
dharmangbhavsar <dharmangb@gmail.com>
2018-03-12 02:26:15 -0400

    Adding another sequence matching ALGORITHM.

---
+++ new_sequence_matching.py
@@ -0,0 +1,24 @@
+import difflib
+search = "Empire State Building"
+text = "Empire Building is an excellent Place to be!!!"    
+words = text.split()
+word = search.split()
+tweet_words = len(words)
+query_words = len(word)
+all_words = []
+for i in range(0,tweet_words):
+	t = ""
+	for j in range(0,query_words):
+		if((i+j)<tweet_words):
+			t += words[i+j]
+			t+=" "
+	all_words.append(t)
+
+print(all_words)
+print(difflib.get_close_matches(search, all_words, cutoff=0.7))
+print("------------------")
+#three = [' '.join([i,j,k]) for i,j,k in zip(words, words[1:], words[2:])]
+#print(three)
+#print(difflib.get_close_matches('no information available', three, cutoff=0.9))
+#Oyutput:
+#['n0 inf0rmation available']
